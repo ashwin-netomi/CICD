0c016ebad1a92c5467bf161c154b9240b8b7beda
ashwin-netomi <ashwin.krishna@netomi.com>
2022-06-15 17:37:57 +0530

    lint issue fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,21 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   "root": true,
-  "extends": [
+  'extends': [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
   "env": {
-    "vue/setup-compiler-macros": true
+    "vue/setup-compiler-macros": true,
   },
   "overrides": [
     {
       "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
+        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}",
       ],
       "extends": [
-        "plugin:cypress/recommended"
+        "plugin:cypress/recommended",
       ]
     }
   ]
